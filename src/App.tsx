da7b1ee50bd12c6c8291232a2f043d68b95515d8
Ilya Kuznecov <wierwolf@inbox.ru>
2023-04-16 22:14:46 +0500

    testing api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./App.module.css";
 import nightSkyVideo from "./assets/videos/BlackSky.mp4";
 import daySkyVideo from "./assets/videos/Sky.mp4";
-import Access from "./Weather.js";
+import axios from "axios";
+import Weather from "./Weather.js";
 
 function App() {
+    type WeatherApiData = {
+        current: {
+            temp_c: number;
+            condition: {
+                icon: string;
+            };
+        };
+    };
+
+    const [url, setUrl] = useState("");
+
+    useEffect(() => {
+        GetWeather();
+    }, []);
+
+    async function GetWeather() {
+        const response = await axios.get<WeatherApiData>(
+            `http://api.weatherapi.com/v1/current.json?key=${Weather.data}&q=London&aqi=no`
+        );
+
+        setUrl(response.data.current.condition.icon);
+        console.log(response.data.current.temp_c);
+        console.log(response);
+    }
     return (
         <div className={styles.App}>
+            <img src={url} />
         </div>
     );
 }
