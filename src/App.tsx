f2f8b02e0769ac99c6d5464729defe281dc385cc
Ilya Kuznecov <wierwolf@inbox.ru>
2023-04-16 21:55:33 +0500

    add api key

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from "./App.module.css";
 import nightSkyVideo from "./assets/videos/BlackSky.mp4";
 import daySkyVideo from "./assets/videos/Sky.mp4";
+import Access from "./Weather.js";
 
 function App() {
     return (
         <div className={styles.App}>
-
         </div>
     );
 }
