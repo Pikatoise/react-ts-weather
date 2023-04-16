852a83366b76fc9c476dcdc43375961f66666702
Ilya Kuznecov <wierwolf@inbox.ru>
2023-04-16 18:24:35 +0500

    import background videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from "./App.module.css";
+import nightSkyVideo from "./assets/videos/BlackSky.mp4";
+import daySkyVideo from "./assets/videos/Sky.mp4";
 
 function App() {
     return (
         <div className={styles.App}>
+
         </div>
     );
 }
