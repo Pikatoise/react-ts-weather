169ffb6bd58b408c4236cffa9bb7bd6ab564ae16
Ilya Kuznecov <wierwolf@inbox.ru>
2023-04-16 22:15:19 +0500

    remove testing api

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,10 @@ import axios from "axios";
 import Weather from "./Weather.js";
 
 function App() {
-    type WeatherApiData = {
-        current: {
-            temp_c: number;
-            condition: {
-                icon: string;
-            };
-        };
-    };
-
-    const [url, setUrl] = useState("");
-
-    useEffect(() => {
-        GetWeather();
-    }, []);
-
-    async function GetWeather() {
-        const response = await axios.get<WeatherApiData>(
-            `http://api.weatherapi.com/v1/current.json?key=${Weather.data}&q=London&aqi=no`
-        );
-
-        setUrl(response.data.current.condition.icon);
-        console.log(response.data.current.temp_c);
-        console.log(response);
-    }
+    
     return (
         <div className={styles.App}>
-            <img src={url} />
+            
         </div>
     );
 }
